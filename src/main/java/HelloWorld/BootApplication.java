67368c6a37fb54c11ec1b16de262dd522010f95b
CJMatx <connormatthews@cmail.carleton.ca>
2017-02-22 00:52:17 -0500

    Added hello world package to test out Travis CI

---
+++ src/main/java/HelloWorld/BootApplication.java
@@ -0,0 +1,15 @@
+package HelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by connor on 2/21/17.
+ */
+
+@SpringBootApplication
+public class BootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+}
