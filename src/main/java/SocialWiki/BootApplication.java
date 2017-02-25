3a4974efa5cd07cb5b6b7a24ecd9f528d37ec8d6
CJMatx <connormatthews@cmail.carleton.ca>
2017-02-25 01:04:18 -0500

    [#5] Removed HelloWorld boot application and removed the user repo prefill code

--- src/main/java/SocialWiki/BootApplication.java
+++ src/main/java/SocialWiki/BootApplication.java
@@ -1,8 +1,5 @@
 package SocialWiki;
 
-import SocialWiki.Users.User;
-import SocialWiki.Users.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,30 +14,4 @@ public class BootApplication {
         SpringApplication.run(BootApplication.class, args);
     }
 
-    //@Bean
-    public CommandLineRunner demo(UserRepository repo) {
-        return (args) -> {
-            repo.save(new User("testUserName", "testFirstName", "testLastName", "testEmail", "testPassword"));
-            repo.save(new User("CJMatx", "Connor", "Matthews", "connor.matthews@carleton.ca", "password123"));
-
-            User user1 = new User();
-            user1.setUserName("testUserName1");
-            user1.setFirstName("testFirstName1");
-            user1.setLastName("testLastName1");
-            user1.setEmail("testEmail1");
-            user1.setPassword("testPassword1");
-
-            repo.save(user1);
-
-            System.out.println("----- All users -----");
-            for(User user : repo.findAll()) {
-                System.out.println(user.toString());
-            }
-
-            System.out.println("----- Filtered by username: testUserName -----");
-            for(User user : repo.findByUserName("testUserName")) {
-                System.out.println(user.toString());
-            }
-        };
-    }
 }
