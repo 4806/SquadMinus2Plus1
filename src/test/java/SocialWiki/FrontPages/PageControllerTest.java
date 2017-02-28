5af5ea9d337d5073acf4c4c95a89797ac44044e6
David <davidbriglio@cmail.carleton.ca>
2017-02-28 11:16:01 -0500

    [#19] Removed unused imports.

--- src/test/java/SocialWiki/FrontPages/PageControllerTest.java
+++ src/test/java/SocialWiki/FrontPages/PageControllerTest.java
@@ -8,10 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
@@ -28,13 +26,11 @@ public class PageControllerTest {
     @Test
     public void hello() throws Exception {
         this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk());
-                //.andExpect(content().string(containsString("hello")));
     }
 
     @Test
     public void login() throws Exception {
         this.mockMvc.perform(get("/login")).andDo(print()).andExpect(status().isOk());
-                //.andExpect(content().string(containsString("login")));
     }
 
 }
