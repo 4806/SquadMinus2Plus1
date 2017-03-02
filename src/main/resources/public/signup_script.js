adf782f61af7f750ae34885c1e781b79cb613773
David <davidbriglio@cmail.carleton.ca>
2017-03-02 11:14:56 -0500

    #28 Fixed sending request after failed validation.

--- src/main/resources/public/signup_script.js
+++ src/main/resources/public/signup_script.js
@@ -13,56 +13,51 @@ signupHandler.submitHandler = function( ) {
     if ( !name || !last || !user || !pass || !passConfirm || !email ) {
         signupHandler.errorHandler("Signup Failed. Please fill all fields.");
     }
-
     // Check to make sure the password and confirm matches
-    if ( pass !== passConfirm ) {
+    else if ( pass !== passConfirm ) {
         signupHandler.errorHandler("Signup Failed. Password does not match password confirm.");
     }
-
     // Check for valid email format
-    if ( !pageUtil.validateEmail(email) ){
+    else if ( !pageUtil.validateEmail(email) ){
         signupHandler.errorHandler("Signup Failed. Invalid email.");
     }
-
     // Make sure username is valid format
-    if( !pageUtil.validateAlphaNum(user) ) {
+    else if( !pageUtil.validateAlphaNum(user) ) {
         signupHandler.errorHandler("Signup Failed. Invalid username (only alphanumeric accepted).");
     }
-
     // Make sure name is valid format
-    if( !pageUtil.validateAlphaNum(name) ) {
+    else if( !pageUtil.validateAlphaNum(name) ) {
         signupHandler.errorHandler("Signup Failed. Invalid name (only alphanumeric accepted).");
     }
-
     // Make sure last name is valid format
-    if( !pageUtil.validateAlphaNum(last) ) {
+    else if( !pageUtil.validateAlphaNum(last) ) {
         signupHandler.errorHandler("Signup Failed. Invalid last name (only alphanumeric accepted).");
     }
-
     // Make sure password is valid format
-    if( !pageUtil.validateAlphaNum(pass) ) {
+    else if( !pageUtil.validateAlphaNum(pass) ) {
         signupHandler.errorHandler("Signup Failed. Invalid password (only alphanumeric accepted).");
     }
-
-    webix.ajax().post("/signup", $$("signup_form").getValues(), {
-        error:signupHandler.errorResponseHandler,
-        success:function(dataString){
-            if( dataString === null ) {
-                signupHandler.errorResponseHandler();
+    else {
+        webix.ajax().post("/signup", $$("signup_form").getValues(), {
+            error:signupHandler.errorResponseHandler,
+            success:function(dataString){
+                if( dataString === null ) {
+                    signupHandler.errorResponseHandler();
+                }
+                var data = JSON.parse(dataString);
+                var userName = $$("user").getValue();
+                if( data !== null && ( data.userName === userName || data.email === userName ) ) {
+                    $$("signup_form").hide();
+                    $$("signup_top_label").hide();
+                    $$("signup_result").css = "label_text";
+                    $$("signup_result").setValue("Signup Succeeded.");
+                    webix.alert("User Signup Successful.", signupHandler.signupSuccessHandler);
+                } else {
+                    signupHandler.errorResponseHandler();
+                }
             }
-            var data = JSON.parse(dataString);
-            var userName = $$("user").getValue();
-            if( data !== null && ( data.userName === userName || data.email === userName ) ) {
-                $$("signup_form").hide();
-                $$("signup_top_label").hide();
-                $$("signup_result").css = "label_text";
-                $$("signup_result").setValue("Signup Succeeded.");
-                webix.alert("User Signup Successful.", signupHandler.signupSuccessHandler);
-            } else {
-                signupHandler.errorResponseHandler();
-            }
-        }
-    });
+        });
+    }
 };
 
 signupHandler.errorHandler = function(message) {
