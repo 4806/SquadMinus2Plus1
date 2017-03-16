3a46eaa069b1d981c3d4c23960cba3501cfa228f
David <davidbriglio@cmail.carleton.ca>
2017-03-16 12:13:26 -0400

    #39 Added tests for login_script.js

---
+++ src/test/javascript/login_script.test.js
@@ -0,0 +1,29 @@
+/**
+ * Created by david on 16/03/17.
+ */
+describe("login_script", function(){
+
+    loginHandler.onReady();
+    $$("user").setValue("test");
+
+    // Testing the proper user being returned
+    it("Successful login", function(){
+      loginHandler.successHandler('{"userName":"test", "email":"test@test.test"}');
+      expect($$("login_result").getValue()).toBe(loginHandler.strings.success);
+    });
+
+    // Testing a response of a different user + a null response
+    it("Invalid login", function(){
+      loginHandler.successHandler('{"userName":"test_error", "email":"test@test.test"}');
+      expect($$("login_result").getValue()).toBe(loginHandler.strings.error);
+
+      loginHandler.successHandler(null);
+      expect($$("login_result").getValue()).toBe(loginHandler.strings.error);
+    });
+
+    // Testing an error in response
+    it("Unsuccessful login", function(){
+      loginHandler.errorHandler();
+      expect($$("login_result").getValue()).toBe(loginHandler.strings.error);
+    });
+});
