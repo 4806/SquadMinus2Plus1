d8e7a1fc9bc4446085854d6fc57e9590a82fd269
David <davidbriglio@cmail.carleton.ca>
2017-04-03 18:39:46 -0400

    #128 User notifications frontend added (no tests).

--- src/main/resources/public/general_page.js
+++ src/main/resources/public/general_page.js
@@ -64,6 +64,7 @@ generalPages.toolbar = {
 
     //Right
     {view:"button", id:"toolbarnewpage", value:"New Page", align:"right", width:100, click:generalPages.handler.addPageClick},
+    {view:"icon", id:"toolbarnotificationbutton", icon:"envelope", align:"right", popup:"notificationPopup"},
     {view:"button", id:"toolbaruserbutton", value:generalPages.getCookie("user"), align:"right", width:100, click:generalPages.handler.userClick},
     {view:"button", id:"toolbarlogin", value:"Login", align:"right", width:100, click:generalPages.handler.loginClick},
     {view:"button", id:"toolbarlogout", value:"Logout", align:"right", width:100, click:generalPages.handler.logoutClick},
@@ -71,13 +72,65 @@ generalPages.toolbar = {
   ]
 };
 
+generalPages.clearNotifications = function() {
+  $$("toolbarnotificationbutton").define("badge", 0);
+  $$("toolbarnotificationbutton").refresh();
+  $$("notificationList").clearAll();
+  $$("notificationList").add({"content":"No new notifications."});
+
+  //Request notification clear
+  // webix.ajax().post("/removeAllUserNotifications", {});
+};
+
 generalPages.formatToolbar = function() {
+
+  //Setup toolbar buttons
   if(generalPages.getCookie("user") === null) {
     $$("toolbaruserbutton").hide();
+    $$("toolbarnotificationbutton").hide();
     $$("toolbarlogout").hide();
     $$("toolbarnewpage").hide();
   } else {
     $$("toolbarsignup").hide();
     $$("toolbarlogin").hide();
+
+    //Add popup element
+    webix.ui({
+      view:"popup",
+      id:"notificationPopup",
+      head:"Submenu",
+      width:300,
+      autoheight:true,
+      on:{
+        onHide:generalPages.clearNotifications
+      },
+      body:{
+        view:"list",
+        id:"notificationList",
+    		data:[ {"content":"No new notifications."}],
+    		datatype:"json",
+        type:{
+          height:62
+        },
+    		template:"#content#"
+    	}
+    });
+
+    // Set notifications
+    webix.ajax().get("/getUserNotifications", {
+      success:function(data){
+        if(data !== null && data !== "[]"){
+          realData = JSON.parse(data);
+          if(realData.length > 0){
+            $$("toolbarnotificationbutton").define("badge", realData.length);
+            $$("toolbarnotificationbutton").refresh();
+            $$("notificationList").clearAll();
+            for(var index in realData){
+              $$("notificationList").add({"content":realData[index]});
+            }
+          }
+        }
+      }
+    });
   }
 };
